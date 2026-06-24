cd123eda8707e1fb0a506b745d6bb75592141183
Luke Lemongello <llemon0105@gmail.com>
2026-06-24 15:14:33 -0400

    Add CLI user input

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Option 1 - Specify your own cards:
-//        ArrayList<Card> p1 = new ArrayList<>();
-//        p1.add(new Card(14, "Spades"));  // Ace of Spades
-//        p1.add(new Card(14, "Clubs"));   // Ace of Clubs
-
-        // Option 2 - Deal random hole cards (comment out Option 1 and uncomment this):
-//         Deck deck = new Deck();
-//         deck.shuffle();
-//         ArrayList<Card> p1 = new ArrayList<>();
-//         p1.add(deck.dealCard());
-//         p1.add(deck.dealCard());
-
         Scanner reader = new Scanner(System.in);
         ArrayList<Card> p1 = new ArrayList<>();
         for(int i = 1; i <= 2; i ++){
